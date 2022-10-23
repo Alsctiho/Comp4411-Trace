0d076ca4a4234ee8bd0afbe6c0c7d255a50590be
Lauren <chchandm@ust.hk>
2022-10-23 18:26:02 +0800

    box intersection

--- RaySkelS20/src/SceneObjects/Box.cpp
+++ RaySkelS20/src/SceneObjects/Box.cpp
@@ -7,6 +7,60 @@ bool Box::intersectLocal( const ray& r, isect& i ) const
 {
 	// YOUR CODE HERE:
     // Add box intersection code here.
-	// it currently ignores all boxes and just returns false.
-	return false;
+
+	double tMin = -1.0e308; // 1.0e308 is close to infinity... close enough for us!
+	double tMax = 1.0e308;
+
+	BoundingBox localBoundingBox = this->ComputeLocalBoundingBox();
+
+	if (localBoundingBox.intersect(r, tMin, tMax))
+	{
+		i.obj = this;
+		i.t = tMin;
+		i.N = r.at(tMin).normalize();
+		return true;
+	}
+	else return false;
+	/*
+	for (int currentaxis = 0; currentaxis < 3; currentaxis++)
+	{
+
+		
+		double vd = Rd[currentaxis];
+		double v0 = R0[currentaxis];
+		double vl = localBoundingBox.min[currentaxis];
+		double vh = localBoundingBox.max[currentaxis];
+
+		// if the ray is parallel to the face's plane (=0.0)
+		if (vd == 0.0)
+			//continue;
+		{
+			if (v0 < vl || v0 > vh) return false;
+		}
+
+		// double v1 = min[currentaxis] - R0[currentaxis];
+		// double v2 = max[currentaxis] - R0[currentaxis];
+
+		// two slab intersections
+		double t1 = (vl - v0) / vd;
+		double t2 = (vh - v0) / vd;
+
+		if (t1 > t2) { // swap t1 & t2 since t1 intersection with near plane 
+			ttemp = t1;
+			t1 = t2;
+			t2 = ttemp;
+		}
+
+		if (t1 > tMin)  // want largest tMin
+			tMin = t1;
+		if (t2 < tMax)	// want smallest tMax
+			tMax = t2;
+
+		if (tMin > tMax) // box is missed
+			return false;
+		if (tMax < 0.0) // box is behind ray
+			return false;
+
+	}
+	*/
 }
